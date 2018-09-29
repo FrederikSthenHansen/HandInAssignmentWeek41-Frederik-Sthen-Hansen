5b396224af3fbe39053926a52911c7b437e55741
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2018-09-29 12:27:33 +0200

    Weight ConverterClassLibrary+tcpConvertServer added

---
+++ TcpConvertServer/TcpConvertServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TcpConvertServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
